43538d4a41167c36cf090405b46bafac99fe1f14
David <davidinholee@gmail.com>
2018-03-18 14:38:02 -0400

    Updated random forest to include a grid search

--- scripts/random-forest-classifier.py
+++ scripts/random-forest-classifier.py
@@ -3,29 +3,32 @@ from sklearn.metrics import accuracy_score
 from sklearn.utils import shuffle
 import numpy as np
 
-# Loading and shuffling the data
 save_dir = "../data/clf_data/sim/"
 samples = np.load(save_dir+"training_data.npz")
 data_2d = samples['images']
 targets = samples['label']
-data_2d, targets = shuffle(data_2d, targets, random_state=0)
-data = np.empty([100000, 1024])
 
-for i in range(0, 100000):
-    data[i] = data_2d[i].ravel()
+for estimators in [500, 1000]:
+    for nodes in [8, 16, 24, 32, 40]:
+        for depth in [4, 8, 12, 16, 20]:
+            data_2d, targets = shuffle(data_2d, targets, random_state=0)
+            data = np.empty([100000, 1024])
 
-# Splitting the data
-train_data = data[:80000]
-train_targets = targets[:80000]
+            for i in range(0, 100000):
+                data[i] = data_2d[i].ravel()
 
-test_data = data[80000:]
-test_targets = targets[80000:]
+            # Splitting the data
+            train_data = data[:80000]
+            train_targets = targets[:80000]
+
+            test_data = data[80000:]
+            test_targets = targets[80000:]
 
 
-clf = RandomForestClassifier(n_estimators=500, max_leaf_nodes=16, n_jobs=-1)
-clf.fit(train_data, train_targets)
+            clf = RandomForestClassifier(n_estimators=estimators, max_leaf_nodes=nodes, max_depth=depth, n_jobs=-1)
+            clf.fit(train_data, train_targets)
 
-test_pred = clf.predict(test_data)
-acc = accuracy_score(test_targets, test_pred)
+            test_pred = clf.predict(test_data)
+            acc = accuracy_score(test_targets, test_pred)
 
-print(acc)
+            print("Estimators: " + str(estimators) + " Nodes: " + str(nodes) + " Depth: " + str(depth) + " Accuracy: " + str(acc))
