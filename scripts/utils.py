b231bf7feb7bb27307b6c8386e5747f5971febcc
Jae Hyeon Lee <jaekor91@gmail.com>
2018-03-24 22:42:20 -0400

    Added generalized PSF.

--- scripts/utils.py
+++ scripts/utils.py
@@ -32,6 +32,30 @@ def gauss_PSF(num_rows, num_cols, x, y, FWHM):
 
     return PSF
 
+def generalized_gauss_PSF(num_rows, num_cols, x, y, FWHM, rho=0, num_comps=10, scatter = 0):
+    """
+    Given num_rows x num_cols of an image, generate a generalized PSF
+    at location x, y.
+    
+    - Rho: covariance element of the 2D covariance matrix with sigma as diagonal std.
+    - num_comps: Number of components overwhich to divide up the components.
+    - scatter: The scatter in row and column direction.
+    
+    bivariate formula: https://en.wikipedia.org/wiki/Multivariate_normal_distribution
+    """
+    sigma = FWHM / 2.354
+    im = np.zeros((num_rows, num_cols))
+    xv = np.arange(0.5, num_rows)
+    yv = np.arange(0.5, num_cols)
+    yv, xv = np.meshgrid(xv, yv) # In my convention xv corresponds to rows and yv to columns
+    
+    for _ in xrange(num_comps):
+        dx, dy = np.random.randn(2) * scatter
+        PSF = np.exp(-(np.square(xv-x-dx) + np.square(yv-y-dy) - 2 * rho * (yv-y-dy) * (xv - x -dx))/(2*sigma**2 * (1-rho**2))) \
+            /(np.pi * 2 * sigma**2 * np.sqrt(1 - rho**2))
+
+    return PSF    
+
 def poisson_realization(D0):
     """
     Given a truth image D0, make a Poisson realization of it.
