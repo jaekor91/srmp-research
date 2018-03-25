0dc3f2ca2ffec07df87694b52b36a812dc81acc5
Jae Hyeon Lee <jaekor91@gmail.com>
2018-03-25 11:59:10 -0400

    Altered the script so that peaks can be off centered

--- scripts/blob_sim_generator.py
+++ scripts/blob_sim_generator.py
@@ -37,16 +37,18 @@ for i in xrange(Nsample):
     r = np.random.random()
     if (r > 0.5) and (r < 0.75): # Single
         label = 1
+        x = np.random.randint(nrows//2 - 5, nrows//2 + 5, 1)[0] # Wavelength position
         y = np.random.randint(2, ncols-2, 1)[0] # Wavelength position
         f = (fmax - fmin) * np.random.random() + fmin # Random flux selection
         rho = (rho_max - rho_min) * np.random.random() + rho_min # Covarince selection
         FWHM = (FWHM_max - FWHM_min) * np.random.random() + FWHM_min # FWHM selection
         num_comps = np.random.randint(1, num_comps_max, 1)[0]
         scatter = np.random.random() * scatter_max
-        peak = f * generalized_gauss_PSF(nrows, ncols, nrows//2,  y, FWHM=FWHM, rho=rho, scatter=scatter, num_comps=num_comps) # Double peak
+        peak = f * generalized_gauss_PSF(nrows, ncols, x,  y, FWHM=FWHM, rho=rho, scatter=scatter, num_comps=num_comps) # Double peak
         im += peak # Add peak
     elif (r > 0.75): # Double
         label = 2
+        x = np.random.randint(nrows//2 - 5, nrows//2 + 5, 1)[0] # Wavelength position        
         y = np.random.randint(5, ncols-5, 1)[0] # Wavelength position of the center
         sep_peaks = np.random.random() * (sep_peaks_max - sep_peaks_min) + sep_peaks_min # Separation in peaks in pixels
         f = (fmax - fmin) * np.random.random() + fmin # Random flux selection
@@ -54,8 +56,8 @@ for i in xrange(Nsample):
         FWHM = (FWHM_max - FWHM_min) * np.random.random() + FWHM_min # FWHM selection
         num_comps = np.random.randint(1, num_comps_max, 1)[0]
         scatter = np.random.random() * scatter_max
-        peak1 = f * generalized_gauss_PSF(nrows, ncols, nrows//2,  y-sep_peaks/2., FWHM=FWHM, rho=rho, scatter=scatter, num_comps=num_comps) # Double peak
-        peak2 = f * generalized_gauss_PSF(nrows, ncols, nrows//2,  y+sep_peaks/2., FWHM=FWHM, rho=rho, scatter=scatter, num_comps=num_comps) # Double peak    
+        peak1 = f * generalized_gauss_PSF(nrows, ncols, x,  y-sep_peaks/2., FWHM=FWHM, rho=rho, scatter=scatter, num_comps=num_comps) # Double peak
+        peak2 = f * generalized_gauss_PSF(nrows, ncols, x,  y+sep_peaks/2., FWHM=FWHM, rho=rho, scatter=scatter, num_comps=num_comps) # Double peak    
         im += (peak1 + peak2) # Add peak
     else:
         label = 0
@@ -96,24 +98,24 @@ label_arr = data["label"]
 sky_added_arr = data["skylines"]
 
     
-# # ---- View a sample of images.
-# plt.close()
-# fig, ax_list = plt.subplots(6, 6, figsize=(10, 10))
+# ---- View a sample of images.
+plt.close()
+fig, ax_list = plt.subplots(6, 6, figsize=(10, 10))
 
-# i_start = 0
-# i_end = i_start + 36
-# for i in range(i_start, i_end):
-#     idx_row = (i-i_start) // 6
-#     idx_col = (i-i_start) % 6
-#     ax_list[idx_row, idx_col].imshow(im_arr[i, :, :], cmap="gray", interpolation="none") # , vmin=vmin, vmax=vmax)
-#     if sky_added_arr[i]:
-#         sky_added = "Sky"
-#     else:
-#         sky_added = ""
-#     title_str = "%4d%10s" % (label_arr[i], sky_added)
-#     ax_list[idx_row, idx_col].set_title(title_str)
-#     ax_list[idx_row, idx_col].axis("off")    
+i_start = 0
+i_end = i_start + 36
+for i in range(i_start, i_end):
+    idx_row = (i-i_start) // 6
+    idx_col = (i-i_start) % 6
+    ax_list[idx_row, idx_col].imshow(im_arr[i, :, :], cmap="gray", interpolation="none") # , vmin=vmin, vmax=vmax)
+    if sky_added_arr[i]:
+        sky_added = "Sky"
+    else:
+        sky_added = ""
+    title_str = "%4d%10s" % (label_arr[i], sky_added)
+    ax_list[idx_row, idx_col].set_title(title_str)
+    ax_list[idx_row, idx_col].axis("off")    
 
-# plt.savefig("blob_sim_training_examples.png", dpi=200, bbox_inches="tight")
+plt.savefig("blob_sim_training_examples.png", dpi=200, bbox_inches="tight")
 # plt.show()
-# plt.close()
+plt.close()
